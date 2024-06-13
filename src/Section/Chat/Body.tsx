ecb01b6aeb19717cc583d4b34adf8f562cd5e5cf
Ihtisham <ihtishamhassan1111@gmail.com>
2024-06-13 16:28:30 +0500

    add the body section in the chat

--- src/Section/Chat/Body.tsx
+++ src/Section/Chat/Body.tsx
@@ -1,6 +1,63 @@
+import { useRef, useState } from "react"
+import MessageBox from "./MessageBox";
+import avatar from "@/assets/avatar.jpg"
+const initialValues = [
+    {
+      id: 1,
+      sender: 'Alice Johnson',
+      content: 'Hey! How are you doing?',
+      timestamp: '2024-06-12T10:00:00Z',
+      avatar: {avatar},
+    },
+    {
+      id: 2,
+      sender: 'Bob Smith',
+      content: 'I am doing well, thanks! How about you?',
+      timestamp: '2024-06-12T10:02:00Z',
+      avatar: {avatar},
+    },
+    {
+      id: 1,
+      sender: 'Alice Johnson',
+      content: 'I am great, thanks for asking!',
+      timestamp: '2024-06-12T10:03:00Z',
+      avatar: {avatar},
+    },
+    {
+      id: 1,
+      sender: 'Bob Smith',
+      content: 'What have you been up to lately?',
+      timestamp: '2024-06-12T10:05:00Z',
+      avatar: {avatar},
+    },
+    {
+      id: 2,
+      sender: 'Alice Johnson',
+      content: 'Just working on some projects. You?',
+      timestamp: '2024-06-12T10:06:00Z',
+      avatar: {avatar},
+    },
+    {
+      id: 1,
+      sender: 'Bob Smith',
+      content: 'Same here, just trying to keep busy.',
+      timestamp: '2024-06-12T10:08:00Z',
+      avatar: {avatar},
+    },
+  ];
+
+  
 const Body = () => {
+ 
+      const bottomRef =  useRef<HTMLDivElement>(null)
+    const [message] = useState(initialValues)
     return (
-        <div className="flex-1 overflow-y-auto">Body....</div>
+        <div className="flex-1 overflow-y-auto">
+         {message.map((msg,i) => (
+              <MessageBox key={msg.id} data={msg} isLast={i === message.length -1} />
+         ))}
+            <div ref={bottomRef} className="pt-24"/> 
+        </div>
     )
 }
 
