1cb4eb25537545a27bd9335b48e4fb63ab20b619
Ihtisham <ihtishamhassan1111@gmail.com>
2024-06-30 03:23:28 +0500

    add the pages

---
+++ src/pages/friendslist.tsx
@@ -0,0 +1,7 @@
+import AppLayout from "@/layout/AppLayout";
+
+const FriendList = () => {
+    return <h1>hi this is my friend list</h1>
+}
+
+export default AppLayout()(FriendList);
---
+++ src/pages/notifications.tsx
@@ -0,0 +1,8 @@
+import AppLayout from "@/layout/AppLayout";
+const Notifications = () => {
+    return (
+        <h1>hi these are the friends request</h1>
+    )
+}
+
+export default AppLayout()(Notifications);
