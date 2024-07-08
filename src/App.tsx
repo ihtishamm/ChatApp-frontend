52c5f7c3f013b79ada5484cb59934e474ad2523b
Ihtisham <ihtishamhassan1111@gmail.com>
2024-07-08 14:41:34 +0500

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { lazy } from "react"
 import { BrowserRouter,Routes,Route } from "react-router-dom"
  const Home = lazy(()=> import("./pages/Home"))
  const Login = lazy(()=> import("./pages/Login"))
+ const Register = lazy(()=> import("./pages/Register"))
  const Chat = lazy(()=> import("./pages/Chat"))
  const NotFound = lazy(()=> import("./pages/NotFound"))
 function App() {
@@ -10,10 +11,10 @@ function App() {
   return (
      <BrowserRouter>
      <Routes>
-
         <Route path="/" element={<Home/>}/>
        <Route path="/chat/:id" element={<Chat/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
         <Route path="*" element={<NotFound/>}/>
       </Routes>
      </BrowserRouter>
