c1a28fc61018b034a72823162b5e2e13663f32dd
Ihtisham <ihtishamhassan1111@gmail.com>
2024-07-09 04:20:33 +0500

    fetch register user endpoint

---
+++ src/Actions/registeruser.tsx
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const registerUser = async (formData: FormData) => {
+  const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/user/register`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+  return response.data;
+};
