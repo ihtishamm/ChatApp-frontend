72fce3fc0c0819a6eff7ca8e7f66a87e41b00d59
Ihtisham <ihtishamhassan1111@gmail.com>
2024-07-09 02:40:13 +0500

    setup the react toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,37 @@
-
-import { lazy, Suspense } from "react"
-import { QueryClient, QueryClientProvider } from  "@tanstack/react-query"
-import { BrowserRouter,Routes,Route } from "react-router-dom"
-import PrivateRoute from "./components/auth/ProtectedRoutes"
-import AuthProvider from "./context/AuthProvider"
- const Home = lazy(()=> import("./pages/Home"))
- const Login = lazy(()=> import("./pages/Login"))
- const Register = lazy(()=> import("./pages/Register"))
- const Chat = lazy(()=> import("./pages/Chat"))
- const NotFound = lazy(()=> import("./pages/NotFound"))
+import { lazy, Suspense } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PrivateRoute from "./components/auth/ProtectedRoutes";
+import AuthProvider from "./context/AuthProvider";
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const Chat = lazy(() => import("./pages/Chat"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 function App() {
- const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-     <BrowserRouter>
-     <Suspense fallback={<div>Loading...</div>}>
-      <AuthProvider>
-     <Routes>
-     <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route element={<PrivateRoute />}>
-  
-       <Route path="/" element={<Home />} />
-      <Route path="/chat/:id" element={<Chat />} />
-     </Route>
-        <Route path="*" element={<NotFound/>}/>
-      </Routes>
-      </AuthProvider>
-      </Suspense>
-     </BrowserRouter>
-     </QueryClientProvider>
-  )
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          <AuthProvider>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route element={<PrivateRoute />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/chat/:id" element={<Chat />} />
+              </Route>
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+            <ToastContainer />
+          </AuthProvider>
+        </Suspense>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
--- src/pages/friendslist.tsx
+++ src/pages/friendslist.tsx
@@ -1,7 +1,6 @@
-import AppLayout from "@/layout/AppLayout";
 
 const FriendList = () => {
     return <h1>hi this is my friend list</h1>
 }
 
-export default AppLayout()(FriendList);
+export default  FriendList;
