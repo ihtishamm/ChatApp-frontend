eda353ca9751c3b2838ae960020857a994add060
Ihtisham <ihtishamhassan1111@gmail.com>
2024-07-21 16:54:17 +0500

    remove the link from the friends list

--- src/Section/FriendsList/FriendsBox.tsx
+++ src/Section/FriendsList/FriendsBox.tsx
@@ -1,27 +1,17 @@
 
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import clsx from "clsx";
-import { useNavigate } from "react-router-dom";
-import  {startTransition} from "react";
-
-const FriendsBox = ({data, selected}: {data: any, selected: boolean}) => {
-
-    const navigate = useNavigate();
+const FriendsBox = ({data}: {data: any, }) => {
 
      console.log(data)
-    const handleConversationClick = (id: any) => {
-        startTransition(() => {
-            navigate(`/chat/${id}`); // Navigate to the chat/:id route
-          });
-    };
+   
 
     return (
-        <div onClick={() => handleConversationClick(data?.id)}
+        <div
             className={clsx(`
                 w-full relative flex items-center space-x-4 hover:bg-neutral-100 
                 rounded-lg p-4 cursor-pointer transition
-            `,
-            selected ? 'bg-neutral-100' : 'bg-white')}>
+            `)}>
             <Avatar>
                 <AvatarImage src={data?.avatar?.avatar} />
             </Avatar>
