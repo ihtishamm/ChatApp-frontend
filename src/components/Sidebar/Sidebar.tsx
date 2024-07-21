f187fd3f1005cc8bc459fe275cf182b120cf05f9
Ihtisham <ihtishamhassan1111@gmail.com>
2024-07-21 16:39:10 +0500

    pass the props toio the desktop and mobile components for the dynamic content

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,18 +1,23 @@
+import { FC, ReactNode } from "react";
 import DesktopSidebar from "./DesktopSidebar";
 import MobileSidebar from "./MobileSidebar";
 
+interface SidebarProps {
+  children: ReactNode;
+  setActiveComponent: (component: string) => void;
+    activeComponent: string
+}
 
- const Sidebar = ({ children }: { children: React.ReactNode }) =>{
+const Sidebar: FC<SidebarProps> = ({ children, setActiveComponent ,activeComponent}) => {
+  return (
+    <div className="h-full h-screen">
+      <DesktopSidebar setActiveComponent={setActiveComponent} activeComponent={activeComponent} />
+      <MobileSidebar setActiveComponent={setActiveComponent} activeComponent={activeComponent} />
+      <main className="lg:pl-20 h-screen">
+        {children}
+      </main>
+    </div>
+  );
+};
 
-    return (
-        <div className="h-full h-screen">
-            <DesktopSidebar/>
-            <MobileSidebar/>
-            <main className="lg:pl-20 h-screen">
-            {children}
-            </main>
-        </div>
-    )
- }
-
- export default Sidebar;
+export default Sidebar;
