e973869cb048b26d8c63b1e7925384ef9cf840cf
Ihtisham <ihtishamhassan1111@gmail.com>
2024-07-21 16:38:40 +0500

    dynmically load the content in the sidebar

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,18 +1,34 @@
 
 
 import ConversationsList from "@/Section/AllConversationsList/AllConversationsList";
+import NotificationsList from "@/Section/Notifications/NotificationsList";
+import FriendsList from "@/Section/FriendsList/FriendList";
 import Sidebar from "@/components/Sidebar/Sidebar";
 
 import { JSX } from "react/jsx-runtime";
+import { useState } from "react";
 
 
  const AppLayout = () => (WrappedComponent: React.ComponentType) => {
     return (props: JSX.IntrinsicAttributes) => {
+        const [activeComponent, setActiveComponent] = useState<string>("conversations");
+
+        const renderActiveComponent = () => {
+            switch (activeComponent) {
+              case "conversations":
+                return <ConversationsList />;
+              case "notifications":
+                return <NotificationsList />;
+                case "friends":
+                return <FriendsList />;
+              default:
+                return <ConversationsList />;
+            }
+          };
         return (
-            <>
-            <Sidebar>
-                <ConversationsList/>
-                 {/* <NotificationsList/> */}
+            <> 
+            <Sidebar activeComponent={activeComponent} setActiveComponent={setActiveComponent} >
+                   {renderActiveComponent()}
                 <div className="h-full h-screen">
                 <WrappedComponent {...props} />
             </div>
