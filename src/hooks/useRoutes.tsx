9e57f7df398bab248c18da2fa4c6567e04820caa
Ihtisham <ihtishamhassan1111@gmail.com>
2024-07-21 16:38:09 +0500

    change the useRoute hook according to the active state prop

--- src/hooks/useRoutes.tsx
+++ src/hooks/useRoutes.tsx
@@ -4,38 +4,33 @@ import { HiChat } from "react-icons/hi";
 import { IoMdNotifications } from "react-icons/io";
 import { FaUserFriends } from "react-icons/fa";
 import useConversation from "./useConversation";
-import { useAuth } from "@/context/AuthProvider";
 
-const useRoutes = () => {
-     const { logOut } = useAuth();
-    const location = useLocation();
-    const pathname = location.pathname;
-    const { conversationId } = useConversation();
+const useRoutes = (activeComponent:string) => {
+  const location = useLocation();
+  const pathname = location.pathname;
+  const { conversationId } = useConversation();
 
-      const friendRequests = 1;
-    const routes = useMemo(() => [
-        {
-            label:"Chat",
-            href:"/conversations",
-            icon: HiChat,
-            active: pathname === "/conversations" || !!conversationId
-        },
-        {
-            label:"Notifications",
-            href:"/notifications",
-            icon: IoMdNotifications,
-            active: pathname === "/notifications",
-            badge: friendRequests > 0 ? friendRequests : undefined
-        },
-        {
-            label:"Friends",
-            href:"/friends",
-            icon: FaUserFriends,
-            active: pathname === "/friends"
-        },
-    ],[pathname, conversationId]);
-    
-     return routes;
-}
+  const friendRequests = 1;
+  const routes = useMemo(() => [
+    {
+      label: "Conversations",
+      icon: HiChat,
+      active: activeComponent === "conversations"
+    },
+    {
+      label: "Notifications",
+      icon: IoMdNotifications,
+      active: activeComponent === "notifications",
+      badge: friendRequests > 0 ? friendRequests : undefined,
+    },
+    {
+      label: "Friends",
+      icon: FaUserFriends,
+      active: activeComponent === "friends"
+    },
+  ], [activeComponent, pathname, conversationId]);
+
+  return routes;
+};
 
 export default useRoutes;
