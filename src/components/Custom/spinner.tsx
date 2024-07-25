da4e85e884f0986bd805138bfa58a87510d5bf83
Ihtisham <ihtishamhassan1111@gmail.com>
2024-07-25 14:25:14 +0500

    exhance the spinner component

--- src/components/Custom/spinner.tsx
+++ src/components/Custom/spinner.tsx
@@ -2,6 +2,9 @@
 
 export const Spinner = () => {
   return (
+    <div className="flex justify-center items-center h-full">
+             
     <div className="w-8 h-8 border-4 border-black border-dashed rounded-full animate-spin"></div>
+    </div>
   );
 };
