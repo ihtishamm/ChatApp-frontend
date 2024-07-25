504fe507d30b8162a7a4a659162353267abe2b74
Ihtisham <ihtishamhassan1111@gmail.com>
2024-07-25 14:26:30 +0500

    intergrate the submit handler in the form of  sendMessage component

--- src/Section/Chat/MessageInput.tsx
+++ src/Section/Chat/MessageInput.tsx
@@ -1,10 +1,19 @@
-const MessageInput = () => {
+import { ChangeEvent } from "react";
+
+interface MessageInputProps {
+    value: string;
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  }
+
+const MessageInput:React.FC<MessageInputProps> = ({value, onChange}) => {
     return (
        <div className="relative w-full">
         <input placeholder="send the message...."
         className="text-black 
          font-light py-2 px-4 w-full rounded-full bg-neutral-200 focus:outline-none transition"
-        
+          value={value}
+          onChange={onChange}
+          type="text"
         />
        </div>
     )
--- src/Section/Chat/SendMessage.tsx
+++ src/Section/Chat/SendMessage.tsx
@@ -1,14 +1,23 @@
 import  { IoIosAttach } from "react-icons/io";
 import MessageInput from "./MessageInput";
 import { IoMdSend } from "react-icons/io";
-const SendMessage = () => {
+import { ChangeEvent, FormEvent } from "react";
+
+interface SendMessageProps {
+  message: string;
+  setMessage: (message: string) => void;
+  submitHandler: (e: FormEvent) => void;
+}
+
+
+const SendMessage:React.FC<SendMessageProps> = ({message,setMessage,submitHandler}) => {
     return (
           <div 
           className="
            py-4 px-4 border-t  flex items-center gap-2 lg:gap-4 w-full
           "><IoIosAttach size={38} className="text-neutral-500"/>
-           <form className="flex items-center gap-2 lg:gap-4 w-full"> 
-             <MessageInput/>
+           <form className="flex items-center gap-2 lg:gap-4 w-full" onSubmit={submitHandler}> 
+             <MessageInput value={message} onChange={(e: ChangeEvent<HTMLInputElement>) => setMessage(e.target.value)}/>
              <button
              type="submit" className="
               rounded-full
