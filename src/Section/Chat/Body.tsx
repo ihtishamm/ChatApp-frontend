81e4a34cc5ef2787b9fab04b28c764986e85fc1a
Ihtisham <ihtishamhassan1111@gmail.com>
2024-08-01 12:00:41 +0500

    fix minor things

--- src/Section/Chat/Body.tsx
+++ src/Section/Chat/Body.tsx
@@ -1,23 +1,28 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef } from "react";
 import MessageBox from "./MessageBox";
- 
-const Body = ({messages}) => {
 
-     useEffect(() => {  
-        bottomRef.current?.scrollIntoView({ behavior: "smooth" });
-    }
-    ,[messages])
+const Body = ({ messages }) => {
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
 
-      const bottomRef =  useRef<HTMLDivElement>(null)
-    
-    return (
-        <div className="flex-1 overflow-y-auto">
-         {messages.map((msg,i) => (
-              <MessageBox key={msg._id} data={msg} isLast={i === messages.length -1} />
-         ))}
-            <div ref={bottomRef} className="pt-16"/> 
-        </div>
-    )
-}
+  const bottomRef = useRef<HTMLDivElement>(null);
 
-export default Body
+  return (
+    <div className="flex-1 overflow-y-auto">
+      {messages?.length === 0 && (
+        <div className="text-center text-gray-500 mt-10">No messages yet</div>
+      )}
+      {messages.map((msg, i) => (
+        <MessageBox
+          key={msg._id}
+          data={msg}
+          isLast={i === messages.length - 1}
+        />
+      ))}
+      <div ref={bottomRef} className="pt-16" />
+    </div>
+  );
+};
+
+export default Body;
